30cb803836bb3259e76f2cb62f7e6587f9859455
Janindumaleesha <janindu.maleesha@app360.lk>
2025-09-07 14:08:08 +0530

    completed delete.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello Viraji!')
 })
 
+// Get all users.
 app.get('/api/users', async (req, res) => {
     try {
         const users = await User.find({})
@@ -18,6 +19,7 @@ app.get('/api/users', async (req, res) => {
     }
 })
 
+// Get user by id.
 app.get('/api/user/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -28,10 +30,47 @@ app.get('/api/user/:id', async (req, res) => {
     }
 })
 
+// Create new user.
 app.post('/api/user', async (req, res) => {
     try {
         const user = await User.create(req.body)
         res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// Update exists user.
+app.put('/api/user/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const user = await User.findByIdAndUpdate(id, req.body)
+
+        if (!user) {
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        const updateUser = await User.findById(id)
+
+        res.status(200).json(updateUser);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// Delete exists user.
+app.delete('/api/user/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const user = await User.findByIdAndDelete(id)
+
+        if (!user) {
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        res.status(200).json({message: 'User deleted successfully!'});
     } catch (error) {
         res.status(500).json({message: error.message})
     }
